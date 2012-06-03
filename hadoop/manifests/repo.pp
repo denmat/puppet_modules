760782693ec4b34cad9fa56748caf991861dd115
root <root@puppet.mylocal>
2012-06-04 00:38:33 +1000

    adding makecache

--- hadoop/manifests/repo.pp
+++ hadoop/manifests/repo.pp
@@ -1,7 +1,12 @@
 class hadoop::repo {
   $hadoop_repos            = hiera('yumrepos::cloudera')
-  $hadoop_repos_defaults   = { enabled => 1 }
+  $hadoop_repos_defaults   = { enabled => 1, notify => Exec['makecache'] }
 
   # create repositories
   create_resources(yumrepo, $hadoop_repos, $hadoop_repos_defaults)
+
+  exec {"makecache":
+    command => '/usr/bin/yum makecache',
+    refreshonly => true,
+  }
 }
