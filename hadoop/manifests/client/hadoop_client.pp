66e43da62a20cd23dd4eabc1f837d3d470525b81
DenMat <tu2Bgone@gmail.com>
2012-06-04 11:49:07 +1000

    fixes for client

--- hadoop/manifests/client/hadoop_client.pp
+++ hadoop/manifests/client/hadoop_client.pp
@@ -5,7 +5,9 @@ class hadoop::client::hadoop_client {
   User  <| tag == 'hadoop_node' |> ->
   File  <| tag == 'hadoop_default_dirs' |> -> 
   File  <| tag == 'common_config_files' |> -> 
-  Exec  <| tag == 'common_execs' |> 
+  Exec  <| tag == 'common_execs' |>  ->
+
+  class {"hadoop::client::cluster_config_files": }
 
 
 } #end class
